52a73cfac09d2437737945bc67ac52fc5bf9683e
Jarrod Bellmore <jarrod_bellmore@glic.com>
2017-05-22 17:37:06 -0400

    Basic react app setup with webpack dev server hot module reloading working.

--- app/index.js
+++ app/index.js
@@ -1,7 +1,27 @@
-function component () {
-    var element = document.createElement("div");
-    element.innerHTML = "<span>Hello Guardian!</span>"
-    return element;
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-document.body.appendChild(component());
+import { AppContainer } from 'react-hot-loader';
+// AppContainer is a necessary wrapper component for HMR
+
+import App from './components/App.jsx';
+
+// This function wraps the initialization since it must be initialized
+// differently if hot module replacement should be enabled
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component/>
+    </AppContainer>,
+    document.getElementById('root')
+  );
+};
+
+render(App);
+
+// Enable HMR at root level using Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./components/App.jsx', () => {
+    render(App)
+  });
+}
